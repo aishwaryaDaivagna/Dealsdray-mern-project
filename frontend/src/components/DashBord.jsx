0a37ea01f7c4cfe5ddc051375f7f76944c3b1c47
aishwaryaDaivagna <aishwaryaud24@gmail.com>
2024-11-12 17:09:28 +0530

    some changes in frontend and backend

--- frontend/src/components/DashBord.jsx
+++ frontend/src/components/DashBord.jsx
@@ -18,17 +18,17 @@ const DashBord = () => {
 
   return (
     <div>
-      <div id='navbar' className='bg-gray-300  '>
+      <div id='navbar' className='p-1.5 bg-lime-500'>
         <ul className='flex gap-24 px-10'>
-          <li>Home</li>
-          <li><Button variant="text"><Link to='/create-employee'> Create Employee</Link></Button> </li>
+          <li className='p-1'>Home</li>
+          <li><Button variant="text"><Link to='/create-employee'> Add Employee</Link></Button> </li>
           <li><Button variant="text"><Link to="/employee-list">  Employee list </Link></Button> </li>
-          <li className='p-2 text-red-500 border border-dashed border-red-400 '>{name}</li>
-          <li>Logout</li>
+          <li className='p-1 text-sky-700 border border-dashed border-red-400 '>{name}</li>
+          <li className='p-1'>Logout</li>
         </ul>
       </div>
-      <h1 className='bg-yellow-200'>DashBord</h1>
-      <p>Wellcome to admin panel</p>
+      <h1 className='p-1.5 bg-lime-300 text-sky-700 indent-10 text-xl'>Dashboard</h1>
+      <p className='bg-white indent-10 font-serif p-1.5 text-6xl h-screen flex items-stretch'>Welcome to admin panel {name}</p>
     </div>
   )
 }
